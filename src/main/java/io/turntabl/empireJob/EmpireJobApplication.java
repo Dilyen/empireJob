4e5bf389b5575e1cbc05a669992cd197eea8ada3
Dilyen <davidandohacquah@gmail.com>
2020-06-19 10:14:32 -0400

    initial commit

---
+++ src/main/java/io/turntabl/empireJob/EmpireJobApplication.java
@@ -0,0 +1,68 @@
+package io.turntabl.empireJob;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+@EnableSwagger2
+@SpringBootApplication
+@EnableScheduling
+
+public class EmpireJobApplication {
+	@Autowired
+	JdbcTemplate template;
+
+	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+		SpringApplication.run(EmpireJobApplication.class, args);
+	}
+
+	public static List<EndpointTO> parseJson(String json) throws IOException {
+		ObjectMapper map = new ObjectMapper();
+		List<EndpointTO> myObjects = Arrays.asList(map.readValue(json, EndpointTO[].class));
+		return myObjects;
+
+	}
+
+	@Scheduled(fixedDelay = 300000, initialDelay = 60000) // delay for 1 min on start and run after every 2min
+		public static void empireJobCode() throws URISyntaxException, IOException, InterruptedException {
+		// run job code
+		String endpoints_location = System.getenv("GET_ENDPOINTS_LIST_URL");
+		System.out.println(System.getenv("GET_ENDPOINTS_LIST_URL"));
+		HttpClient client = HttpClient.newBuilder().build();
+		try{
+			URI url;
+			url = new URI(System.getenv("GET_ENDPOINTS_LIST_URL"));
+
+			HttpRequest request = HttpRequest.newBuilder(url).build();
+			HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.defaultCharset()));
+			jobProcess.deleteData();
+			parseJson(res.body()).stream().forEach(e -> {
+				jobProcess.getstatus(e.getProject_id(), e.getEndpoint_url(), e.getRequest_method(), e.getEndpoint_id());
+
+			});
+
+	} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/io/turntabl/empireJob/jobProcess.java
@@ -0,0 +1,75 @@
+package io.turntabl.empireJob;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class jobProcess {
+    public static void getstatus(Integer project_id,String url, String method, Integer endpoint_id){
+
+        String result = "";
+        int code;
+        try{
+            URL siteURL = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+            connection.setRequestMethod(method);
+            connection.setConnectTimeout(3000);
+            connection.connect();
+
+            code = connection.getResponseCode();
+            postData(endpoint_id, project_id, code);
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void  postData(Integer endpoint_id, Integer project_id, Integer status ) throws IOException, InterruptedException {
+        Map<String, Integer> values = new HashMap<String, Integer>() {{
+            put("endpoint_id", endpoint_id);
+            put("project_id", project_id);
+            put("status", status);
+
+        }};
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writeValueAsString(values);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(System.getenv("ADD_ENDPOINT_STATUS_URL")))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
+
+    public static void deleteData() throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(System.getenv("DELETE_STATUS_URL")))
+                .header("Content-Type", "application/json")
+                .DELETE()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    }
+    }
